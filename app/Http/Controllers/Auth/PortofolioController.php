cd7efe16aa14ad654e810b0afccfd1d74ba1af8a
Ruslan <galinskirus@gmail.com>
2024-10-25 00:53:45 +0500

    test cloud

--- app/Http/Controllers/Auth/PortofolioController.php
+++ app/Http/Controllers/Auth/PortofolioController.php
@@ -58,8 +58,8 @@ class PortofolioController extends Controller
     {
         $user = Auth::user();
         $userGallery = json_decode($user->gallery, true);
-        dd(Str::contains($request->file('file'), ["https://promob.s3.amazonaws.com/", "https://dspt7sohnkg6q.cloudfront.net/"]), $request->file('file'));
-        if (Str::contains($request->file('file'), ["https://promob.s3.amazonaws.com/", "https://dspt7sohnkg6q.cloudfront.net/"])) {
+        dd(Str::contains($request->fileName, ["https://promob.s3.amazonaws.com/", "https://dspt7sohnkg6q.cloudfront.net/"]), $request->fileName);
+        if (Str::contains($request->fileName, ["https://promob.s3.amazonaws.com/", "https://dspt7sohnkg6q.cloudfront.net/"])) {
             $cleanPath = str_replace(["https://promob.s3.amazonaws.com/", "https://dspt7sohnkg6q.cloudfront.net/"], "", $request->fileName);
             if (in_array($request->fileName, $userGallery)) {
                 $result = array_diff($userGallery, [$request->fileName]);
