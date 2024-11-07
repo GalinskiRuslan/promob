b690495d8f638d34bae5e09a25be0dbd21ffe6ed
Ruslan Issapov <r_issapov@almaty.gamma.kz>
2024-11-07 17:01:49 +0500

    Доработки

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,7 @@ Route::post('/deleteAccount', [ApiAuthController::class, 'deleteAccount']);
 // Изменеие портфолио и информации о пользователе
 Route::get('/getUserInfo', [ApiUserController::class, 'getUserInfo']);
 Route::post('/savePortfolioItem', [ApiPortfolioController::class, 'savePortfolioItem']);
+Route::post('/deletePortfolioItem', [ApiPortfolioController::class, 'deletePortfolioItem']);
 Route::post('/editUserInfo', [ApiUserController::class, 'editUserInfo']);
 Route::post('/uploadAvatar', [ApiUserController::class, 'uploadAvatar']);
 Route::post('/updateContacts', [ApiUserController::class, 'changeContactsUser']);
