506f15713e7566d6d9c912072e2909a324be7cb3
Ruslan <galinskirus@gmail.com>
2024-11-23 00:41:17 +0500

    some fesh

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -37,6 +37,7 @@ class ApiAuthController extends Controller
                 $verificationCode = str_pad(random_int(0, 9999), 4, '0', STR_PAD_LEFT);
                 $code_id = Str::uuid();
                 $smsService = new SmsController();
+                dd($smsService, $code_id);
             } catch (\Exception $e) {
                 return response()->json(['message' => $e->getMessage()], 400);
             }
