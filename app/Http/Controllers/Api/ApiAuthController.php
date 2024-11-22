40f21f4861a35c066e03797c3caa5a0e194b0fb2
Ruslan <galinskirus@gmail.com>
2024-11-23 01:01:41 +0500

    some fesh

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -28,9 +28,8 @@ class ApiAuthController extends Controller
         } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 400);
         }
-        if (true) {
+        if (User::whereRaw("REPLACE(REPLACE(REPLACE(REPLACE(tel, ' ', ''), '(', ''), ')', ''), '-', '') = ?", [$request->tel])->first() || !User::whereRaw("REPLACE(REPLACE(REPLACE(REPLACE(tel, ' ', ''), '(', ''), ')', ''), '-', '') = ?", [$request->tel])->first()->is_verified) {
             $existingCode = VerifySms::where('tel', '+' . $request->tel)->first();
-            dd($existingCode);
             if ($existingCode && $existingCode->updated_at->diffInMinutes(now()) < 3) {
                 return response()->json(['message' => 'Код можно отправить только раз в 3 минуты'], 400);
             }
